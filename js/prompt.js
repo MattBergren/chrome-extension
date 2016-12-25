a07db52afb12a5af7a81047d9efbae2932bee6e1
Matthew Bergren <matt@Hal.local>
2016-12-25 08:29:34 -0600

    adding timestamp to message

---
+++ js/prompt.js
@@ -0,0 +1,3 @@
+var userName = prompt('Hello, what\'s your name?');
+
+document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' + '<p>' + projectName + ' ' + versionNumber + ' accecessed on:' + currentTime + '</p>';
---
+++ js/values.js
@@ -0,0 +1,5 @@
+var projectName = 'kittenbook';
+var versionNumber = '0.0.1';
+var currentDate = new Date(); // Create Date object.
+var currentTime = currentDate.getFullYear() + '-' +(currentDate.getMonth() + 1) + '-' +currentDate.getDate() + ' at ' +currentDate.getHours() + ':' +currentDate.getMinutes() + ':' +currentDate.getSeconds();
+
