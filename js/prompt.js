d53aad607f7d94e578277adc1c2ff12af3084ce4
Matt Bergren <mbergren@horizontalintegration.com>
2016-12-29 14:21:58 -0600

    adding build

--- js/prompt.js
+++ js/prompt.js
@@ -1,11 +1,2 @@
 var userName = prompt('Hello, what\'s your name?');
-document.body.innerHTML = '<h1>Hello, ' 
-						+ userName 
-						+ '!</h1>' 
-						+ '<p>' 
-						+ projectName 
-						+ ' ' 
-						+ versionNumber + 
-						' accecessed on: ' 
-						+ currentTime 
-						+ '</p>';
+document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' + '<p>' + projectName + ' ' + versionNumber + ' accecessed on: ' + currentTime + '</p>';
