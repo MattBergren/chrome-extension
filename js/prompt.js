def5dff35cdc3fbf5a36179e6a5517b0ea4183c4
Matt Bergren <mbergren@horizontalintegration.com>
2017-01-02 14:19:38 -0600

    adding phone number question and kittenbook ojbect

--- js/prompt.js
+++ js/prompt.js
@@ -1,2 +1,17 @@
 var userName = prompt('Hello, what\'s your name?');
-document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' + '<p>' + projectName + ' ' + versionNumber + ' accecessed on: ' + currentTime + '</p>';
+var phoneNumber = prompt('Hello ' + userName + ' what\'s your phone number?');
+var phoneNumberPattern = /1?-?\(?\d{3}[\-\)]\d{3}-\d{4}/;
+
+var output = '<h1>Hello, ' + userName + '!</h1>';
+
+// Is the phone number valid? 
+if (phoneNumberPattern.test(phoneNumber)) {
+	// Yes, the phone number is valid! Add the success message to the output. 
+	output = output + '<p>' + kbValues.projectName + ' ' + kbValues.versionNumber + ' viewed on: ' + kbValues.currentTime + '</p>';
+} else {
+	// No, the phone number is not valid. Tell the user about the problem. 
+	output = output + '<h2>That phone number is invalid: ' + phoneNumber; 
+}
+
+// Insert the output into the web page. 
+document.body.innerHTML = output;
--- js/values.js
+++ js/values.js
@@ -1,5 +1,7 @@
-var projectName = 'kittenbook';
-var versionNumber = '0.0.1';
 var currentDate = new Date(); // Create Date object.
-var currentTime = currentDate.getFullYear() + '-' +(currentDate.getMonth() + 1) + '-' +currentDate.getDate() + ' at ' +currentDate.getHours() + ':' +currentDate.getMinutes() + ':' +currentDate.getSeconds();
 
+var kbValues = {
+	projectName: 'kittenbook',
+	versionNumber: '0.0.1',
+	currentTime: currentDate.getFullYear() + '-' +(currentDate.getMonth() + 1) + '-' +currentDate.getDate() + ' at ' +currentDate.getHours() + ':' +currentDate.getMinutes() + ':' +currentDate.getSeconds()
+}
